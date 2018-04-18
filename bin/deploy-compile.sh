99ad537e830a87beab2ae1eab0abe1ed8d4475a0
Nick Whyte <nick@nickwhyte.com>
2018-04-18 17:18:07 +1000

    arm32 -> arm
    Why golang, why?

--- bin/deploy-compile.sh
+++ bin/deploy-compile.sh
@@ -7,7 +7,7 @@ VERSION=$(git describe --dirty --always)
 LDFLAGS="-X main.Version=$VERSION"
 
 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-amd64
-GOOS=linux GOARCH=arm32 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-arm32
+GOOS=linux GOARCH=arm go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-arm
 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-arm64
 GOOS=linux GOARCH=mips64 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-mips64
 GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/collect-owners-darwin-amd64
