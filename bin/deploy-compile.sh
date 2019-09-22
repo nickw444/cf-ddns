c0abf7111c5276d5693e0b77084fc6b3bbc4b8e8
Nick Whyte <nick@nickwhyte.com>
2019-09-22 14:43:13 +1000

    Fix release binary naming

--- bin/deploy-compile.sh
+++ bin/deploy-compile.sh
@@ -6,9 +6,9 @@ mkdir -p target
 VERSION=$(git describe --dirty --always)
 LDFLAGS="-X main.Version=$VERSION"
 
-GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-amd64
-GOOS=linux GOARCH=arm go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-arm
-GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-arm64
-GOOS=linux GOARCH=mips64 go build -ldflags "$LDFLAGS" -o target/collect-owners-linux-mips64
-GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/collect-owners-darwin-amd64
-GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/collect-owners-windows-amd64
+GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/cf-ddns-linux-amd64
+GOOS=linux GOARCH=arm go build -ldflags "$LDFLAGS" -o target/cf-ddns-linux-arm
+GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -o target/cf-ddns-linux-arm64
+GOOS=linux GOARCH=mips64 go build -ldflags "$LDFLAGS" -o target/cf-ddns-linux-mips64
+GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/cf-ddns-darwin-amd64
+GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o target/cf-ddns-windows-amd64
